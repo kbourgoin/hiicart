8307612556e1a5e726af5de52c3152782c318d7e
Keith Bourgoin <keith.bourgoin@gmail.com>
2011-01-08 22:55:15 -0500

    Index view shows status of payment gateways.

--- example/apps/basic/views.py
+++ example/apps/basic/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from django.conf import settings
+from django.shortcuts import render_to_response
+from pprint import PrettyPrinter
+
+from hiicart import validate_gateway
+
+def index(request):
+    p = PrettyPrinter(width=40)
+    context = {
+        'paypal': (p.pformat(settings.HIICART_SETTINGS['PAYPAL']),
+                   validate_gateway('paypal')),
+        'paypal2': (p.pformat(settings.HIICART_SETTINGS['PAYPAL2']),
+                   validate_gateway('paypal2')),
+        'paypal_adaptive': (p.pformat(settings.HIICART_SETTINGS['PAYPAL_ADAPTIVE']),
+                   validate_gateway('paypal_adaptive')),
+        'google': (p.pformat(settings.HIICART_SETTINGS['GOOGLE']),
+                   validate_gateway('google')),
+        'amazon': (p.pformat(settings.HIICART_SETTINGS['AMAZON']),
+                   validate_gateway('amazon')),
+        }
+    return render_to_response('index.html', context)
