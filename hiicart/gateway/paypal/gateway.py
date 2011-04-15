5b5d79c407fc7f1556dc8215f69fd25ec5a298bc
Ray Slakinski <ray.slakinski@gmail.com>
2011-04-15 17:31:17 -0400

    fixing indent issue

--- hiicart/gateway/paypal/gateway.py
+++ hiicart/gateway/paypal/gateway.py
@@ -163,7 +163,7 @@ class PaypalGateway(PaymentGatewayBase):
                     submit["a2"] = item.trial_price
                     submit["p2"] = item.trial_length
                     submit["t2"] = item.duration_unit
-            else:
+            # else:
                 # Messes up trial periods, so only use if no trial/delay
                 # submit["modify"] = "1"  # new or modify subscription
             # subscription price
