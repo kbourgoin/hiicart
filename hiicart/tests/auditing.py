b0620bd5e4e2c7026da8fd9d8ef25b2948685746
Jason Moiron <jmoiron@jmoiron.net>
2011-05-02 16:05:20 -0400

    add some fixes for the testing suite;  there's still a couple test failures, but the unit test suite functions again;  included some workarounds for two tests that fail due to leap year issues that I'm not sure are important to the underlying functionality of hiicart, added an auditing test stub

---
+++ hiicart/tests/auditing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""test auditing facilities"""
+
+from unittest import TestCase
+
+class AuditingTestCase(TestCase):
+
+    def test_auditing(self):
+        pass
+
