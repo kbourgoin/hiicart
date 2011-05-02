9c54c81affaf527aa6c7048dc69523cc610476d2
Jason Moiron <jmoiron@jmoiron.net>
2011-05-02 17:29:52 -0400

    add a facility to hiicart that allows us to log sentry error messages with full stack traces from the logging location

--- hiicart/tests/auditing.py
+++ hiicart/tests/auditing.py
@@ -3,10 +3,23 @@
 
 """test auditing facilities"""
 
-from unittest import TestCase
+from unittest import TestCase, main
+from hiicart.lib import auditing
+from django.views.debug import ExceptionReporter
 
 class AuditingTestCase(TestCase):
-
     def test_auditing(self):
-        pass
+        def foo():
+            return bar()
+        def bar():
+            return baz()
+        def baz():
+            return auditing.FakeTraceback()
+        tb = foo()
+        er = ExceptionReporter(None, type, None, tb)
+        # if this fails we've messed up the fake traceback
+        html = er.get_traceback_html()
 
+
+if __name__ == '__main__':
+    main()
