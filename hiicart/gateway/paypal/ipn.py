eeb2340f9c226af93560492ed5d9ed11a86ecf0c
Jason Moiron <jmoiron@jmoiron.net>
2011-05-16 15:22:05 -0400

    change our logging from info => debug, add self.cart.save() to each update_state call

--- hiicart/gateway/paypal/ipn.py
+++ hiicart/gateway/paypal/ipn.py
@@ -60,6 +60,7 @@ class PaypalIPN(IPNBase):
         if data.get("note", False):
             payment.notes.create(text="Comment via Paypal IPN: \n%s" % data["note"])
         self.cart.update_state()
+        self.cart.save()
 
     def activate_subscription(self, data):
         """Send signal that a subscription has been activated."""
@@ -76,6 +77,7 @@ class PaypalIPN(IPNBase):
             item.is_active = True
             item.save()
             self.cart.update_state()
+            self.cart.save()
 
     def cancel_subscription(self, data):
         """Send signal that a subscription has been cancelled."""
@@ -89,6 +91,7 @@ class PaypalIPN(IPNBase):
             item.is_active = False
             item.save()
             self.cart.update_state()
+            self.cart.save()
 
     def confirm_ipn_data(self, raw_data):
         """Confirm IPN data using string raw post data.
